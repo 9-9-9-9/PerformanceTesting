208a961031b87408cf06a5c787059a1208679ebf
HungPV <hungpv@goquo.com>
2020-04-23 08:47:08 +0700

    'update'

--- SharedLib/RandomUtils.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace SharedLib
-{
-    public static class RandomUtils
-    {
-        public static string RandomLabel => $"L{Guid.NewGuid().ToString().Substring(0, 4)}";
-    }
-}
